6dd72c874bded9fd0356b311f561b76de956f8fc
Adam Schmideg <adam@schmideg.net>
2018-05-10 12:14:32 +0200

    Make testing method throw Throwable

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class LibraryTest {
-    @Test public void testSomeLibraryMethod() {
+    @Test public void testSomeLibraryMethod() throws Throwable {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
